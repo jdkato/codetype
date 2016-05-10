8c430f3222242b0811671498fcf3cf54ba200266
Joseph Kato <jdkato24@gmail.com>
2016-05-10 11:52:18 -0700

    Start adding base tests to sigmgr

--- cypher/sigmgr.py
+++ cypher/sigmgr.py
@@ -13,31 +13,38 @@ parser.add_argument(
     nargs="?",
     help="Language name"
 )
+parser.add_argument(
+    "-t",
+    "--test",
+    action="store_true",
+    help="Run base test"
+)
 
 TEMP_DIR = os.path.join(os.getcwd(), "cypher", "temp")
 if os.path.exists(TEMP_DIR):
     shutil.rmtree(TEMP_DIR)
-lang = vars(parser.parse_args())["lang"]
-if lang == "Python":
+
+args = vars(parser.parse_args())
+if args["lang"] == "Python":
     repo = "https://github.com/django/django.git"
     ext = [".py"]
-elif lang == "Ruby":
+elif args["lang"] == "Ruby":
     repo = "https://github.com/Homebrew/legacy-homebrew.git"
     ext = [".rb"]
-elif lang == "C":
+elif args["lang"] == "C":
     repo = "https://github.com/git/git.git"
     ext = [".c", ".h"]
-elif lang == "C++":
+elif args["lang"] == "C++":
     repo = "https://github.com/apple/swift.git"
     ext = [".cpp", ".cc", ".h"]
-elif lang == "R":
+elif args["lang"] == "R":
     repo = "https://github.com/rstudio/shiny.git"
     ext = [".R", ".r"]
-elif lang == "Haskell":
+elif args["lang"] == "Haskell":
     repo = "https://github.com/haskell/cabal.git"
     ext = [".hs"]
 else:
-    print("{} not found.".format(lang))
+    print("{} not found.".format(args["lang"]))
     sys.exit(0)
 
 os.makedirs(TEMP_DIR)
@@ -50,5 +57,8 @@ pro = subprocess.Popen(
 (out, error) = pro.communicate()
 
 src_dir = os.path.join(TEMP_DIR, repo.split("/")[-1].split(".")[0])
-write_signature(src_dir, lang=lang, ext=ext, is_file=1)
+if args["test"]:
+    pass
+else:
+    write_signature(src_dir, lang=args["lang"], ext=ext, is_file=1)
 shutil.rmtree(TEMP_DIR)
