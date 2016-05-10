3ff9c60cc82d9e801039b0a2894816a424b7525f
Joseph Kato <jdkato24@gmail.com>
2016-05-09 18:31:46 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "rb") as md:
+    README = md.read().decode("utf-8")
+
+setup(
+    name="cypher",
+    packages=["cypher"],
+    entry_points={"console_scripts": ["cypher=cypher.cypher:main"]},
+    version="0.0.1",
+    description="A Python source code identification tool.",
+    long_description=README,
+    author="Joseph Kato",
+    author_email="jdkato@gmail.com",
+    url=""
+)
