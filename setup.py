e78b337bfb1498dcfe35d1a5ef5653a72e9d6a2a
Joseph Kato <jdkato@users.noreply.github.com>
2016-05-10 10:20:06 -0700

    Remove Python from setup.py description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=["cypher"],
     entry_points={"console_scripts": ["cypher=cypher.cypher:main"]},
     version="0.0.1",
-    description="A Python source code identification tool.",
+    description="A source code identification tool.",
     long_description=README,
     author="Joseph Kato"
 )
