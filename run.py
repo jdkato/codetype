20a925be743476e9d7c600b2d1253feacc643e4b
Joseph Kato <jdkato24@gmail.com>
2016-05-13 15:27:56 -0700

    Add args to run.py

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+import argparse
 import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "script",
+    nargs="?",
+    help="cypher|mkdocs|sigmgr"
+)
 
 if __name__ == "__main__":
     from cypher.cypher import main
