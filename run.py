6b14f879ca478ae2180a34a709cbf25484c6d5b1
Joseph Kato <jdkato24@gmail.com>
2016-05-18 20:59:46 -0700

    Rewrite run.py

--- run.py
+++ run.py
@@ -4,12 +4,34 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "script",
-    nargs="?",
-    const="cypher",
-    type=str,
-    help="cypher|mddocs|sigmgr"
+    nargs=1,
+    help="mddocs|sigmgr|test"
+)
+parser.add_argument(
+    "-t",
+    "--test",
+    action="store_true"
+)
+parser.add_argument(
+    "lang",
+    nargs="?"
 )
 
-if __name__ == "__main__":
+args = vars(parser.parse_args())
+script = args["script"][0]
+if not script:
     from cypher.cypher import main
     sys.exit(main())
+elif script == "sigmgr":
+    from dev.sigmgr import run
+    if args["test"] and args["lang"]:
+        from cypher.util import identify
+        sys.exit(run(lang=args["lang"], is_test=1, identifier=identify))
+    elif args["lang"]:
+        from cypher.util import write_signature as sig_writer
+        sys.exit(run(lang=args["lang"], is_test=0, writer=sig_writer))
+elif script == "test":
+    import unittest
+    suite = unittest.TestLoader().discover(".")
+    run = unittest.TextTestRunner().run(suite)
+sys.exit(0)
