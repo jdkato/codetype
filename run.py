2c9e99192fbdeaade740465d8ceef653ff474863
Joseph Kato <jdkato24@gmail.com>
2016-05-22 10:14:13 -0700

    Move imports

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 import argparse
 import sys
+import unittest
+
+from cypher.util import (
+    identify,
+    write_signature as sig_writer
+)
+from dev.sigmgr import run
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -32,19 +39,13 @@ parser.add_argument(
 args = vars(parser.parse_args())
 script = args["script"][0]
 if script == "sigmgr":
-    from dev.sigmgr import run
     if args["test"] and args["lang"]:
-        from cypher.util import identify
         sys.exit(run(lang=args["lang"], is_test=1, identifier=identify))
     elif args["lang"]:
-        from cypher.util import write_signature as sig_writer
         sys.exit(run(lang=args["lang"], is_test=0, writer=sig_writer))
 elif script == "test":
-    import unittest
-    suite = unittest.TestLoader().discover(".")
-    run = unittest.TextTestRunner().run(suite)
+    unittest.TextTestRunner().run(unittest.TestLoader().discover("."))
 elif script == "cypher":
-    from cypher.util import identify
     r = identify(args["lang"], is_file=args["file"], verbose=args["verbose"])
     print(r)
 else:
