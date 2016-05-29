127eec8d03edf8d92625ce8feb165a0b6a289105
Joseph Kato <jdkato@users.noreply.github.com>
2016-05-29 15:52:18 -0700

    Add test_extract_content

--- test/test_cypher.py
+++ test/test_cypher.py
@@ -2,13 +2,13 @@ import os
 import sys
 import unittest
 
-from cypher.util import identify
+from cypher.util import identify, extract_content
 
 sys.path.insert(0, os.path.abspath("."))
 LANG_DIR = os.path.join("test", "lang")
 
 
-class identifyTestCase(unittest.TestCase):
+class CypherTestCase(unittest.TestCase):
     """
     """
     def test_identify(self):
@@ -22,6 +22,20 @@ class identifyTestCase(unittest.TestCase):
                 if f.endswith(".txt"):
                     computed = identify(os.path.join(subdir, f))
                     self.assertEqual(known, computed)
+                    
+    def test_extract_content(self):
+        """
+        """
+        cases = {
+            "print('I am a string')": "print()",
+            "cout << 'I am another string' << endl;": "cout <<  << endl;",
+            "print('foo')  # Inline comment": "print()",
+            "printf('%d', 2) // Inline comment": "printf(, 2)",
+            "test += 1": "test += 1"
+        }
+        for line, output in cases.items():
+            self.assertEqual(output, extract_content(line))
+            
 
 
 if __name__ == "__main__":
