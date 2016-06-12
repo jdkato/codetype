e4a6663145da71678ae9d9d1b06c6092e04c06f6
Joseph Kato <jdkato24@gmail.com>
2016-06-12 09:50:32 -0700

    Remove extract_content tests

--- test/test_cypher.py
+++ test/test_cypher.py
@@ -2,7 +2,7 @@ import os
 import sys
 import unittest
 
-from cypher.util import identify, extract_content
+from cypher.util import identify
 
 sys.path.insert(0, os.path.abspath("."))
 LANG_DIR = os.path.join("test", "lang")
@@ -22,20 +22,6 @@ class CypherTestCase(unittest.TestCase):
                 if f.endswith(".txt"):
                     computed = identify(os.path.join(subdir, f))
                     self.assertEqual(known, computed)
-                    
-    def test_extract_content(self):
-        """
-        """
-        cases = {
-            "print('I am a string')": "print()",
-            "cout << 'I am another string' << endl;": "cout <<  << endl;",
-            "print('foo')  # Inline comment": "print()",
-            "printf('%d', 2) // Inline comment": "printf(, 2)",
-            "test += 1": "test += 1"
-        }
-        for line, output in cases.items():
-            self.assertEqual(output, extract_content(line))
-            
 
 
 if __name__ == "__main__":
