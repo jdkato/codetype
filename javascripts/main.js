aaeb5f4c8ced55eeb30c03c23fcbd482fd401d34
Joseph Kato <jdkato24@gmail.com>
2016-06-15 11:46:50 -0700

    Add result's presentation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+var identify = function() {
+    $.ajax({
+        type: "GET",
+        url: 'http://localhost:5000/cypher',
+        datatype: 'json',
+        data: { text: $('#text').val() },
+        success: function(data) {
+            langs = data.results;
+            for(var i = 1; i < 4; ++i) {
+                lang = langs[i - 1];
+                if (lang)
+                    $("#lang-" + i).html(lang.replace(/\"/g, ""));
+                else
+                    $("#lang-" + i).html("N\\A");
+            }
+            console.log(data.results);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+};
+
+$(document).ready(function() {
+    var textarea = $("#text");
+    textarea.bind(
+        'input propertychange',
+        _.debounce(identify, 50, { maxWait: 100 })
+    );
+});
