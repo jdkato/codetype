7a9dbfba5ffdae8ca1a1e5f2f1cbd1bf659e6cc9
Joseph Kato <jdkato24@gmail.com>
2016-06-16 10:56:42 -0700

    Handle no results

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,36 @@
+function clearText() {
+    $('#text').val('');
+    $('#inline').html('N/A');
+    $('#block').html('N/A');
+    $('#string').html('N/A');
+    $('#lang-1').html('N/A');
+    $('#lang-2').html('N/A');
+    $('#lang-3').html('N/A');
+}
+
 var identify = function() {
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: 'http://localhost:5000/cypher',
         datatype: 'json',
         data: { text: $('#text').val() },
         success: function(data) {
-            langs = data.results;
-            for(var i = 1; i < 4; ++i) {
-                lang = langs[i - 1];
-                if (lang)
-                    $("#lang-" + i).html(lang.replace(/\"/g, ""));
-                else
-                    $("#lang-" + i).html("N/A");
+            if (_.isEmpty(data)) {
+                clearText();
+            } else {
+                langs = data.results;
+                for(var i = 1; i < 4; ++i) {
+                    lang = langs[i - 1];
+                    if (lang)
+                        $('#lang-' + i).html(lang.replace(/\"/g, ''));
+                    else
+                        $('#lang-' + i).html('N/A');
+                }
+                $('#inline').html(data.inlineCount);
+                $('#block').html(data.blockCount);
+                $('#string').html(data.stringCount);
+                console.log(JSON.stringify(data));
             }
-            $("#inline").html(data.inlineCount);
-            $("#block").html(data.blockCount);
-            console.log(JSON.stringify(data));
         },
         error: function(error) {
             console.log(error);
@@ -24,7 +39,7 @@ var identify = function() {
 };
 
 $(document).ready(function() {
-    var textarea = $("#text");
+    var textarea = $('#text');
     textarea.bind(
         'input propertychange',
         _.debounce(identify, 500, { maxWait: 500 })
