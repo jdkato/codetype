1244887cf10ad52f90a8429b5d8f7e4554f2e6da
Joseph Kato <jdkato24@gmail.com>
2016-06-17 11:03:59 -0700

    Add test for `remove_inline_comment`

--- test/test_cypher.py
+++ test/test_cypher.py
@@ -2,18 +2,19 @@ import os
 import sys
 import unittest
 
-from cypher.util import identify
+from cypher.util import (
+    identify,
+    remove_inline_comment
+)
 
 sys.path.insert(0, os.path.abspath("."))
 LANG_DIR = os.path.join("test", "lang")
 
 
 class CypherTestCase(unittest.TestCase):
-    """
+    """Tests for cypher's utility functions.
     """
     def test_identify(self):
-        """
-        """
         for subdir, dirs, files in os.walk(LANG_DIR):
             known = os.path.basename(subdir)
             if known == "lang":
@@ -25,6 +26,19 @@ class CypherTestCase(unittest.TestCase):
                     computed = identify(os.path.join(subdir, f))
                     self.assertEqual(known, computed)
             print("Tested {} {} files.".format(count, known))
+
+    def test_remove_inline_comment(self):
+        cases = {
+            "var += 1 # This is a comments": "var += 1",
+            "foo # This is a nested -- comment": "foo",
+            "bar // another nested # comment": "bar",
+            "baz /* block-style inline */": "baz",
+            "main() -- comment": "main()",
+            "baz {- block-style inline -}": "baz",
+        }
+        for case, output in cases.items():
+            removed = remove_inline_comment(case)[0].strip()
+            self.assertEqual(removed, output)
 
 
 if __name__ == "__main__":
