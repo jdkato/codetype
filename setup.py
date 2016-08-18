52771402d5f908aa7658b9725c7cf8f77b9ed334
Joseph Kato <jdkato24@gmail.com>
2016-08-18 08:36:20 -0700

    chore: update setup.py for new structure

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup
 
-with open("README.md", "rb") as md:
-    README = md.read().decode("utf-8")
-
 setup(
     name="cypher",
     packages=["cypher"],
-    entry_points={"console_scripts": ["cypher=cypher.cypher:main"]},
+    scripts=["bin/cypher"],
     version="0.0.1",
     description="A source code identification tool.",
     include_package_data=True,
-    long_description=README,
     author="Joseph Kato"
 )
