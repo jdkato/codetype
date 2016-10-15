9115809f81644d1fdc320f4b1db143bbd0e34bbb
Joseph Kato <joseph@jdkato.io>
2016-10-15 10:05:16 -0700

    chore: add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -15,5 +15,11 @@ setup(
     author_email="joseph@jdkato.io",
     url="https://github.com/jdkato/codetype",
     download_url="https://github.com/jdkato/codetype/releases",
-    license='MIT'
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ],
 )
