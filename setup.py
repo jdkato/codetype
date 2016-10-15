2cefe268205eb86744f40db367f2090d69f88296
Joseph Kato <joseph@jdkato.io>
2016-10-15 09:46:42 -0700

    chore: add information to setup.py
    - keywords
    - url
    - download_url

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ setup(
     scripts=["bin/codetype"],
     version="1.0.0",
     description="A source code identification tool.",
+    keywords=["classifying", "identification", "code"],
     install_requires=[
         "msgpack-python"
     ],
     include_package_data=True,
-    author="Joseph Kato"
+    author="Joseph Kato",
+    url="https://github.com/jdkato/codetype",
+    download_url="https://github.com/jdkato/codetype/releases",
 )
