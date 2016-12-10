a52ef0d9748fe2b35428c0c25fb6b428bbb215f4
Joseph Kato <joseph@jdkato.io>
2016-12-10 15:36:37 -0800

    chore: bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="codetype",
     packages=["codetype"],
     scripts=["bin/codetype"],
-    version="1.0.0",
+    version="1.0.1",
     description="A source code identification tool.",
     long_description=long_description,
     keywords=["classifying", "identification", "code"],
